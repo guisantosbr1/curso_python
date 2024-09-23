19cd5f5d848e7cf3f2af6117523ba9b0a5eb5967
guisantosbr1 <guilhermesantos09876@gmail.com>
2024-09-22 22:17:05 -0300

    commit

---
+++ aula20.py
@@ -0,0 +1,7 @@
+valor1 = input('Digite um valor: ')
+valor2 = input('Digite outro valor: ')
+
+if valor1 >= valor2:
+    print(f'O valor {valor1} é maior ou igual o valor {valor2}')
+else:
+    print(f'O valor {valor2} é maior que {valor1}')
