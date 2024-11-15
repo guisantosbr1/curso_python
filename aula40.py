46ac9e1ce6c3ce7757c946741e5ddfb3e7ab7a99
guisantosbr1 <guilhermesantos09876@gmail.com>
2024-11-15 16:22:20 -0300

    calculadora

---
+++ aula40.py
@@ -0,0 +1,52 @@
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    ### Debug: Verificando valores antes da operação
+    ###print(f'Debug - num_1_float: {num_1_float}, num_2_float: {num_2_float}, operador: {operador}')
+
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} =', num_1_float + num_2_float)
+    elif operador == '-' :
+        print(f'{num_1_float} - {num_2_float} =', num_1_float - num_2_float)
+    elif operador == '*' :
+        print(f'{num_1_float} * {num_2_float} =', num_1_float * num_2_float)
+    elif operador == '/' :
+        # Verificação para evitar divisão por zero
+        if num_2_float == 0:
+            print("Erro: Divisão por zero não é permitida.")
+        else:
+            print(f'{num_1_float} / {num_2_float}=', num_1_float / num_2_float)
+    else:
+        print('Erro')
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
